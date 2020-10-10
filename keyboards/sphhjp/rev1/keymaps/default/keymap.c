87e342af2814bdae1a8df8b33827c0681af09a9e
tai02 <taito.yokomizo@88oct.co.jp>
2020-10-10 21:54:10 +0900

    fix: layout of sphhjp

--- keyboards/sphhjp/rev1/keymaps/default/keymap.c
+++ keyboards/sphhjp/rev1/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,    JP_AT,   JP_LBRC, KC_ENT,
     KC_LCTRL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,   KC_K,     KC_L,    JP_SCLN, JP_COLN, JP_RBRC,
     KC_LSHIFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,   JP_COMM,  JP_DOT,  JP_SLSH, JP_BSLS, KC_UP,   KC_RSHIFT,
-    MO(FUNC),  KC_ESC, KC_LALT, KC_LALT, KC_LGUI, KC_SPC,                        KC_BSPC, KC_ENT, KC_LANG1, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+    MO(FUNC),  KC_ESC, KC_LGUI, KC_LALT, KC_LGUI, KC_SPC,                        KC_BSPC, KC_ENT, KC_LANG1, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   [FUNC] = LAYOUT(
     /* Left */                                                                     /* Right */
